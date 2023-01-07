8a1482bac845a50886fe1716666833773367654e
kill_in_sun <noneck.0311@gmail.com>
2023-01-07 16:44:46 +0900

    update

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,8 +2,16 @@ package structs
 
 import "math"
 
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
-	Width float64
+	Width  float64
 	Height float64
 }
 
@@ -20,22 +28,10 @@ func (c Circle) Area() float64 {
 }
 
 type Triangle struct {
-	Base float64
+	Base   float64
 	Height float64
 }
 
 func (t Triangle) Area() float64 {
 	return t.Base * t.Height / 2
-}
-
-type Shape interface {
-	Area() float64
-}
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,7 +2,7 @@ package structs
 
 import "testing"
 
-func TestPerimeter(t *testing.T){
+func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
@@ -13,23 +13,18 @@ func TestPerimeter(t *testing.T){
 }
 
 func TestArea(t *testing.T) {
-
 	areaTests := []struct {
-		name string
 		shape Shape
-		hasArea float64
-	} {
-		{name: "Rectangle",shape: Rectangle{12, 6}, hasArea: 72.0},
-		{name: "Circle", shape: Circle {10}, hasArea: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
-
 	for _, tt := range areaTests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := tt.shape.Area()
-			if got != tt.hasArea{
-				t.Errorf("%#v got %.2f, want %.2f", tt.shape, got, tt.hasArea)
-			}
-		})
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
 	}
 }
